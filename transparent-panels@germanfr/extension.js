2299006e6509393a46096db3b5e67a7d2feab918
Germán Franco <dev.germanfr@gmail.com>
2016-12-04 01:43:01 +0100

    Add license heading

--- transparent-panels@germanfr/extension.js
+++ transparent-panels@germanfr/extension.js
@@ -1,3 +1,22 @@
+/*
+* Transparent panels - Cinnamon desktop extension
+* Transparentize your panels when there are no any maximized windows
+* Copyright (C) 2016  Germán Franco Dorca
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 const GLib = imports.gi.GLib;
 const Mainloop = imports.mainloop;
 const Meta = imports.gi.Meta;
