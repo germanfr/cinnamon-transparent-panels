b26d9cd616c08efb4917d0b6556aff7965efff5b
Germán Franco <dev.germanfr@gmail.com>
2017-02-03 11:45:57 +0100

    Unpdate packaging script to accept symblinks inside zip

--- spices.sh
+++ spices.sh
@@ -3,16 +3,13 @@
 UUID="transparent-panels@germanfr"
 ZIP_NAME="${UUID}.zip"
 
-# Don't add final slash to folders
+# Don't add folders here
 EXTRAS="LICENSE README.md screenshot.png"
 
-if [ -f $ZIP_NAME ] ;then
-	rm $ZIP_NAME
-fi
+rm -f $ZIP_NAME
 
-ln -rfs $EXTRAS $UUID/
-zip -r $ZIP_NAME $UUID
+ln $EXTRAS $UUID/
+zip -r --symlinks $ZIP_NAME $UUID
 
 cd $UUID
 rm $EXTRAS
-
